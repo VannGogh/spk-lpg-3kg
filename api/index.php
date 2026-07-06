ee4218de752a8530629fe54eb960a7bd3585a6b5
Ivan <admin@spklpg.com>
2026-07-06 19:32:29 +0700

    ponytail: fix supabase pdo pooler & vercel filesystem

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
+$_ENV['APP_ENV'] = 'production';
+$_ENV['APP_DEBUG'] = 'false';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['CACHE_DRIVER'] = 'array';
+putenv('SESSION_DRIVER=cookie');
+putenv('VIEW_COMPILED_PATH=/tmp');
 
-// Forward Vercel requests to normal Laravel public/index.php
 require __DIR__ . '/../public/index.php';
