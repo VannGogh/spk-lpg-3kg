d1471e812f2b81b6941720338edd90dc4817e9f9
Ivan <admin@spklpg.com>
2026-07-06 20:37:33 +0700

    fix: CACHE_STORE not CACHE_DRIVER for Laravel 13, add QUEUE_CONNECTION=sync

--- api/index.php
+++ api/index.php
@@ -1,11 +1,10 @@
 <?php
 $_ENV['APP_ENV'] = 'production';
-$_ENV['APP_DEBUG'] = 'true';
+$_ENV['APP_DEBUG'] = 'false';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['LOG_CHANNEL'] = 'stderr';
-$_ENV['CACHE_DRIVER'] = 'array';
-putenv('SESSION_DRIVER=cookie');
-putenv('VIEW_COMPILED_PATH=/tmp');
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['QUEUE_CONNECTION'] = 'sync';
 
 require __DIR__ . '/../public/index.php';
