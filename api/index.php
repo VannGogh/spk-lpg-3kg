2dad8e6def4932b226f632b4d09d4706ab873c2c
Ivan <admin@spklpg.com>
2026-07-06 21:04:37 +0700

    debug: safe exception dump

--- api/index.php
+++ api/index.php
@@ -1,10 +1,20 @@
 <?php
 $_ENV['APP_ENV'] = 'production';
-$_ENV['APP_DEBUG'] = 'false';
+$_ENV['APP_DEBUG'] = 'true';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['CACHE_STORE'] = 'array';
 $_ENV['QUEUE_CONNECTION'] = 'sync';
 
-require __DIR__ . '/../public/index.php';
+putenv('APP_DEBUG=true');
+$_SERVER['APP_DEBUG'] = 'true';
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>CRITICAL ERROR</h1>";
+    $trace = str_replace('IvanPutra@371536', '********', $e->getMessage() . "\n" . $e->getTraceAsString());
+    echo "<pre>" . $trace . "</pre>";
+}
