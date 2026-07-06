22485f7a39b31b4c3cef59471dad4022e5c1471f
Ivan <admin@spklpg.com>
2026-07-06 21:02:21 +0700

    fix: remove DB_URL from pgsql to force individual parameters

--- api/index.php
+++ api/index.php
@@ -1,19 +1,10 @@
 <?php
 $_ENV['APP_ENV'] = 'production';
-$_ENV['APP_DEBUG'] = 'true';
+$_ENV['APP_DEBUG'] = 'false';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['CACHE_STORE'] = 'array';
 $_ENV['QUEUE_CONNECTION'] = 'sync';
 
-putenv('APP_DEBUG=true');
-$_SERVER['APP_DEBUG'] = 'true';
-
-try {
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    http_response_code(500);
-    echo "<h1>CRITICAL ERROR</h1>";
-    echo "<pre>" . $e->getMessage() . "\n" . $e->getTraceAsString() . "</pre>";
-}
+require __DIR__ . '/../public/index.php';
